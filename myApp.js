4beb66937975c133625ec1fe0a1dc18831dc2a47
aryann23 <73698094+aryann23@users.noreply.github.com>
2024-02-16 00:08:42 +0530

    Update myApp.js
    - added filepath index.html to response body

--- myApp.js
+++ myApp.js
@@ -3,8 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
+//- added app.METHOD(PATH, HANDLER) where method = get.
+//path = / (root),
+//handler is the func with req, res body
+
+absolutePath = __dirname + '/views/index.html';
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
   })
 
 
