22fe77afd2aec5c8a5cb21547a311351346ac19a
aryann23 <73698094+aryann23@users.noreply.github.com>
2024-02-17 03:42:49 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
@@ -8,6 +9,11 @@ app.use((req, res, next) => {
   console.log(string);
   next();
 });
+
+//POST is the default method used to send client data with HTML forms. 
+//In REST convention, POST is used to send data to create new items in the database
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 console.log("Hello World");
 
@@ -26,7 +32,8 @@ absolutestaticPath = __dirname + '/public';
 app.use('/public', express.static(absolutestaticPath));
 
 //creating simple api to send JSON - js obj notation
-//When a node application is run, it can check the value of the environment variable and do different things based on the value. NODE_ENV specifically is used (by convention) to state whether a particular environment is a production or a development environment.
+//When a node application is run, it can check the value of the environment variable and do different things based on the value. 
+//NODE_ENV specifically is used (by convention) to state whether a particular environment is a production or a development environment.
 process.env.MESSAGE_STYLE='uppercase';
 app.get('/json', function(req, res) {
     if(process.env.MESSAGE_STYLE==='uppercase'){
@@ -50,7 +57,8 @@ app.get('/:word/echo', function(req, res){
     res.json({'echo': req.params.word})
 });
 
-//adding query parameter input
+//adding query parameter input. Express can parse the data from the query string, and populate the object req.query
+//use route to work on both get and post. The query string is delimited by a question mark (?), and includes field=value couples. 
 app.route('/name').get(function(req, res){
   let first = req.query.first;
   let last = req.query.last;
@@ -58,7 +66,6 @@ app.route('/name').get(function(req, res){
   let jasonObj = {name: `${first} ${last}`};
   res.send(jasonObj);
 }).post();
-
 
 
 
